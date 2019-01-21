349c594e72d2deed62c0fe67fe1644e523b5d805
jenchieh <jcs090218@gmail.com>
2019-01-22 00:04:33 +0800

    Merge to one constant.

---
+++ constant.py
@@ -0,0 +1,20 @@
+# ========================================================================
+# $File: constant.py $
+# $Date: 2019-01-21 23:14:23 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2019 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+HOST = '127.0.0.1'  # Symbolic name meaning all available interfaces
+PORT = 50007        # Arbitrary non-privileged port
+
+BUF_SIZE = 1024
+
+ENCODE_TYPE = 'utf-8'
+DECODE_TYPE = 'utf-8'
+
+# Internal command prefix, must be a character.
+INTERNAL_CMD_PREFIX = '!'
