3de537d65a05b0fd0ee38d5c851ec476725bf5d3
jenchieh <jcs090218@gmail.com>
2019-01-21 23:09:42 +0800

    Add target.

---
+++ target.py
@@ -0,0 +1,18 @@
+# ========================================================================
+# $File: target.py $
+# $Date: 2019-01-21 23:03:50 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2019 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+
+def main():
+    """Program Entry point."""
+
+    
+
+if __name__ == "__main__":
+    main()
