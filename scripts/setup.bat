4351b52f0c63b2a789584c808eec960d753508a0
jenchieh <jcs090218@gmail.com>
2019-01-22 14:42:24 +0800

    Add setup util scripts for setting up the tool correctly.

---
+++ scripts/setup.bat
@@ -0,0 +1,14 @@
+@echo
+:: ========================================================================
+:: $File: setup.bat $
+:: $Date: 2019-01-22 14:31:05 $
+:: $Revision: $
+:: $Creator: Jen-Chieh Shen $
+:: $Notice: See LICENSE.txt for modification and distribution information
+::                   Copyright © 2019 by Shen, Jen-Chieh $
+:: ========================================================================
+
+
+cd ..
+
+pip install -r requirements.txt
---
+++ scripts/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# ========================================================================
+# $File: setup.sh $
+# $Date: 2019-01-22 14:31:32 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2019 by Shen, Jen-Chieh $
+# ========================================================================
+
+
+cd ..
+
+pip install -r requirements.txt
