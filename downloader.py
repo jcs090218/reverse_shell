75e6a5e24ce20ef39b3feba802a3b69dee8c8c5a
jenchieh <jcs090218@gmail.com>
2019-01-22 16:34:57 +0800

    Add downloader module.

---
+++ downloader.py
@@ -0,0 +1,26 @@
+# ========================================================================
+# $File: downloader.py $
+# $Date: 2019-01-22 16:28:56 $
+# $Revision: $
+# $Creator: Jen-Chieh Shen $
+# $Notice: See LICENSE.txt for modification and distribution information
+#                   Copyright © 2019 by Shen, Jen-Chieh $
+# ========================================================================
+
+import urllib.request
+
+
+def download(in_url):
+    """Download file by using URL.
+
+    @param { string } in_url : Input url.
+    """
+    # Get the file name.
+    fileName = os.path.join(os.getcwd(), os.path.basename(in_url))
+    with urllib.request.urlopen(in_url) as infp:
+        with open(fileName, 'wb') as outfp:
+            while True:
+                data = infp.read(16384)
+                if not data:
+                    break
+                outfp.write(data)
